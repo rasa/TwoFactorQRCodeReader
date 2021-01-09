b90e23cd02838eb9e1e233cb568f8146672479e7
Alex Vallat <git@alex.vallat.name>
2021-01-09 18:49:36 +0000

    Update for 2.47 release

--- CreatePlgX.bat
+++ CreatePlgX.bat
@@ -12,7 +12,7 @@ xcopy TwoFactorQRCodeReader PlgX /s /e /exclude:PlgXExclude.txt
 xcopy TwoFactorQRCodeReader\packages\ZXing.Net.0.16.6\lib\net47\*.dll PlgX\packages\ZXing.Net.0.16.6\lib\net47\
 
 echo Compiling PlgX
-"../KeePass/KeePass.exe" /plgx-create "%~dp0PlgX" --plgx-prereq-kp:2.46
+"../KeePass/KeePass.exe" /plgx-create "%~dp0PlgX" --plgx-prereq-kp:2.47
 
 echo Releasing PlgX
 move /y PlgX.plgx "Releases\Build Outputs\TimeOtpQRCodeReader.plgx"
